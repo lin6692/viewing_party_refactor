e8a4c8efc8c8fb31f50bd89d95751731573d1798
Lin Liu <lin_6066@yahoo.com>
2022-04-01 10:08:51 -0700

    define person

--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,3 +1,12 @@
 class Person:
+    def __init__(self, name, watched, friends, subsriptions):
+        
+        pass
+
+    def add_watched(self, movie):
+        pass
+
+    def get_num_watched(self):
+        pass
     pass
     
